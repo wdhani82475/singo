c975fd0c7d947665ad31a1495ec678ed679ced6c
wangdong <wangdong3@cmcm.com>
2020-11-18 17:24:03 +0800

    feat(点赞)

--- api/like/like.go
+++ api/like/like.go
@@ -1,12 +1,7 @@
 package like
 
 import (
-	"encoding/json"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/go-playground/validator"
-	"singo/conf"
-	"singo/serializer"
 	"singo/service/like"
 )
 
@@ -18,36 +13,22 @@ func LikeArticle(c *gin.Context) {
 	var service like.LikeService
 
 	if err := c.ShouldBind(&service);err == nil {
-		res := service.DoLikeArticle()
-		c.JSON(200,res)
+		data := service.DoLikeArticle()
+		c.JSON(200,gin.H{
+			"code":200,
+			"msg":"ok",
+			"data":data,
+		})
 	}else{
-		c.JSON(200, ErrorResponse(err))
+		c.JSON(200, gin.H{
+			"code":200,
+			"msg":"ok",
+			"data":err,
+		})
 	}
-	//返回结果值
-
 }
 
 //取消点赞
 func  DisLikeArticle(c*gin.Context)  {
 
-}
-
-
-
-func ErrorResponse(err error) serializer.Response {
-	if ve, ok := err.(validator.ValidationErrors); ok {
-		for _, e := range ve {
-			field := conf.T(fmt.Sprintf("Field.%s", e.Field))
-			tag := conf.T(fmt.Sprintf("Tag.Valid.%s", e.Tag))
-			return serializer.ParamErr(
-				fmt.Sprintf("%s%s", field, tag),
-				err,
-			)
-		}
-	}
-	if _, ok := err.(*json.UnmarshalTypeError); ok {
-		return serializer.ParamErr("JSON类型不匹配", err)
-	}
-
-	return serializer.ParamErr("参数错误", err)
 }
--- service/like/like_service.go
+++ service/like/like_service.go
@@ -4,66 +4,49 @@ import (
 	"fmt"
 	"singo/model"
 	"singo/model/like"
-	"singo/serializer"
 )
 
 type LikeService struct {
-	PostUser        int `form:"post_user_id" json:"post_user_id" binding:"required"`
-	LikeUser        int `form:"like_user_id" json:"like_user_id" binding:"required"`
-	ArticleId        int `form:"article_id" json:"article_id" binding:"required"`
+	PostUser  int `form:"post_user_id" json:"post_user_id" binding:"required"`
+	LikeUser  int `form:"like_user_id" json:"like_user_id" binding:"required"`
+	ArticleId int `form:"article_id" json:"article_id" binding:"required"`
 }
 
 //1.检测用户是否已经点赞过该文章
-func(service *LikeService) valid() *serializer.Response{
+func (service *LikeService) valid() string {
 	count := 0
-	model.DB.Model(&like.UserLikeArticleModel{}).Where("post_user_id = ? and like_user_id = ? and article_id = ?  ", service.PostUser,service.LikeUser,service.ArticleId).Count(&count)
+	model.DB.Model(&like.UserLikeArticleModel{}).Where("post_user_id = ? and like_user_id = ? and article_id = ?  ", service.PostUser, service.LikeUser, service.ArticleId).Count(&count)
 	fmt.Println(count)
-	if count>0 {
-		return &serializer.Response{
-			Code: 404,
-			Msg:  "您已点过赞",
-		}
+	if count > 0 {
+		return "您已点过赞"
 	}
-	return nil
+	return ""
 }
 
-
-
 //2.文章点赞总数+1
-func (service *LikeService) DoLikeArticle() *serializer.Response {
+func (service *LikeService) DoLikeArticle() string {
 	//校验用户是否点过👍
 	err := service.valid()
-	if err != nil {
+	if err != "" {
 		return err
 	}
 
 	userLikeArticle := like.UserLikeArticleModel{
 		PostUserId: service.PostUser,
 		LikeUserId: service.LikeUser,
-		ArticleId: service.ArticleId,
+		ArticleId:  service.ArticleId,
 	}
 	//用户点赞关联表+1
 	err2 := model.DB.Model(&like.UserLikeArticleModel{}).Create(&userLikeArticle).Error
 	if err2 != nil {
-		return &serializer.Response{
-			Code: 400,
-			Msg:  "插入失败",
-		}
+		return "插入数据失败"
 	}
 	//更新数据
 	var article like.ArticleModel
 	err4 := model.DB.Where("id = ?", service.ArticleId).First(&article).Update("total_like_count", article.TotalLikeCount+1).Error
 	if err4 != nil {
-		return &serializer.Response{
-			Code: 400,
-			Msg:  "更新数据失败",
-		}
+		return "更新数据失败"
 	}
-	return  &serializer.Response{
-		Code: 200,
-		Msg:  "操作成功",
-	}
+	return ""
 }
-
-
 //使用协程同步数据库
